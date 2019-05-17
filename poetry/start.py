4577bf2834a834c4d46c49450cd0af4edbcd0995
Nathan RABET <nathan.rabet@gmail.com>
2019-05-18 01:34:45 +0200

    Modification MAJEURE du DEM_Parser.py et ajout de fonctions pour la gestion des déterminants.

---
+++ poetry/start.py
@@ -0,0 +1,9 @@
+from os import getcwd
+import os
+
+os.environ["POETRY_PATH"] = os.path.dirname(os.path.abspath(__file__))
+
+from C.Class.Parser.DEM_parser import *
+from C.Class.Structure.Determinant import *
+
+print(style_langage("p"))

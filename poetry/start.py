145bfce3605c875bc096cf980491914ec59df24b
Nathan Rabet <nathan.rabet@gmail.com>
2019-05-21 00:28:40 +0200

    Finalisation de l'interface de la console

--- poetry/start.py
+++ poetry/start.py
@@ -3,11 +3,6 @@ import os
 
 os.environ["POETRY_PATH"] = os.path.dirname(os.path.abspath(__file__))
 
-from C.Class.Parser.DEM_parser import *
-from C.Class.Structure.Determinant import *
-from C.Class.Parser.selectionneur_mot import *
-from C.Class.Semantique.Constructeur_phrase import *
 from V.visual import *
 
-
-Afficher_phrase(10,'')
+startup()
